e7d47786665920467b373b8b66b187f5109dfd79
Mateus Araujo <mateusmba20@gmail.com>
2024-05-23 21:43:48 -0300

    feat: implements PUT `/produts/:id` endpoint

---
+++ src/useCase/Products/updateProduct.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { unlink } from 'node:fs';
+import path from 'path';
+import { Product } from '../../models/Product';
+
+export async function updateProduct(req: Request, res: Response) {
+  const { id } = req.params;
+  const imagePath = req.file?.filename;
+  const { name, description, price, category, ingredients} = req.body;
+
+  try {
+    const prevProduct = await Product.findByIdAndUpdate(id, {
+      name,
+      description,
+      price,
+      category,
+      ingredients,
+      imagePath
+    });
+
+    if (prevProduct && imagePath) {
+      unlink(path.resolve(__dirname, '..', '..', '..', 'uploads', `${prevProduct.imagePath}`), (err) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            console.log('updateProduct: Prev image not found');
+            return;
+          }
+
+          console.error('Error when deleting image:', err);
+          return;
+        }
+
+        console.log(`${prevProduct.imagePath} was deleted`);
+      });
+    }
+
+    return res.sendStatus(204);
+  } catch(err) {
+    console.error('Error when updating product:', err);
+    return res.sendStatus(500);
+  }
+}
